3106a30c745ecc4aedf7cb680afd05da30978236
Amrith Shanbhag <amrith@ohme.ga>
2016-12-03 23:17:29 +0000

    Adds copy to clipboard functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 var main = function(){
+    var clipboard = new Clipboard('.color-box');
+    
     $(".color-box").each(function(){
         $(this).css("background-color", $(this).text());
-    });
-    
-    $(".color-box").on('click', function(){
-        
+        $(this).attr("data-clipboard-text",$(this).text());
     });
     
     var width = $('.color-box').width();
@@ -14,6 +13,18 @@ var main = function(){
         var width = $('.color-box').width();
         $('.color-box').css({'height':width+'px', "line-height":width+"px", "font-size":width/6+"px"});
     });
+    
+    
+    clipboard.on('success', function(e) {
+        toastr.success("Color copied to your clipboard", 'Success!');
+    e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        toastr.info("Ctl+C to copy color to clipboard", 'Selected!');
+    });
+   
+    
 };
 
 $(document).ready(main);
