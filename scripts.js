eea6daa0c735a285a7eb28cd1955e9970053ffa0
Amrith Shanbhag <amrith@ohme.ga>
2016-12-05 13:35:04 +0530

    Add colors and branding

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 var main = function(){
     var clipboard = new Clipboard('.color-box');
-    
+
     $(".color-box").each(function(){
         $(this).css("background-color", $(this).text());
         $(this).attr("data-clipboard-text",$(this).text());
     });
-    
+
     var width = $('.color-box').width();
     $('.color-box').css({'height':width+'px', "line-height":width+"px", "font-size":width/6+"px"});
 
@@ -13,18 +13,18 @@ var main = function(){
         var width = $('.color-box').width();
         $('.color-box').css({'height':width+'px', "line-height":width+"px", "font-size":width/6+"px"});
     });
-    
-    
+
+
     clipboard.on('success', function(e) {
-        toastr.success("Color copied to your clipboard", 'Success!');
+        toastr.success("We've put the color on your 🎨", 'Hurray 🎉');
     e.clearSelection();
     });
 
     clipboard.on('error', function(e) {
         toastr.info("Ctl+C to copy color to clipboard", 'Selected!');
     });
-   
-    
+
+
 };
 
 $(document).ready(main);
